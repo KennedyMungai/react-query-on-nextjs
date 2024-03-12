73a47a7c41d7a7ddc33a4f1b691e838c196715d1
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-12 12:15:34 +0300

    Made some changes to the todos page

--- app/todos/page.tsx
+++ app/todos/page.tsx
@@ -1,6 +1,18 @@
+'use client'
+
+import { useMutation } from '@tanstack/react-query'
+import { Todo } from '../page'
+import axios from 'axios'
+
 type Props = {}
 
 const TodosPage = (props: Props) => {
+	const { error, isPending, isSuccess } = useMutation({
+		mutationFn: (newTodo: Todo) => {
+			return axios.post('https://localhost:3001/todos', newTodo)
+		}
+	})
+
 	return <div>TodosPage</div>
 }
 
