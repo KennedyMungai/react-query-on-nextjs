582cca2a8b005133faf774eec8b8ce36acd47024
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-12 11:16:00 +0300

    Added some fetching logic for the jsonplaceholder data

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,25 @@
+'use client'
+
+import { useQuery } from '@tanstack/react-query'
+
 export default function Home() {
-	return <main></main>
+	const { data, isLoading, isError, isSuccess } = useQuery<any>({
+		queryKey: ['todos'],
+		queryFn: () =>
+			fetch('https://jsonplaceholder.typicode.com/todos').then((res) =>
+				res.json()
+			)
+	})
+
+	if (isLoading) {
+		return (
+			<div className='min-h-screen flex items-center justify-center bg-orange-200'>
+				<p className='text-red-500 font-bold text-6xl animate-pulse'>
+					Loading...
+				</p>
+			</div>
+		)
+	}
+
+	return <main className='min-h-screen bg-orange-200'></main>
 }
