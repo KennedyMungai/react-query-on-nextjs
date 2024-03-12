18c78a8e4162cc4d321ae2a2d1f448f3e4ce37a4
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-12 12:23:43 +0300

    Made some changes to the route file

--- app/todos/page.tsx
+++ app/todos/page.tsx
@@ -1,19 +1,40 @@
 'use client'
 
 import { useMutation } from '@tanstack/react-query'
-import { Todo } from '../page'
 import axios from 'axios'
+import { Todo } from '../page'
 
 type Props = {}
 
 const TodosPage = (props: Props) => {
-	const { error, isPending, isSuccess } = useMutation({
+	const { isError, isPending, isSuccess, error, mutate }: any = useMutation({
 		mutationFn: (newTodo: Todo) => {
 			return axios.post('https://localhost:3001/todos', newTodo)
 		}
 	})
 
-	return <div>TodosPage</div>
+	return (
+		<div>
+			{isPending ? (
+				'Adding Todo'
+			) : (
+				<>
+					{isError ? (
+						<div>An error occurred: {error.message}</div>
+					) : null}
+					{isSuccess ? <div>Todo Added</div> : null}
+
+					<button
+						onClick={() =>
+							mutate({ id: new Date(), title: 'Do Laundry' })
+						}
+					>
+						Create Todo
+					</button>
+				</>
+			)}
+		</div>
+	)
 }
 
 export default TodosPage
